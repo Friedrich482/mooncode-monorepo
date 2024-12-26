ec5ed7e71d77de95f5ca93b75d6252b28db8db0c
Friedrich482 <friedricht109@gmail.com>
2024-12-26 21:44:14 +0100

    build: eslint
    added the rules "no-unused-vars" and "sort-import" to the eslint.config.mjs file to make development easier and consistent

--- apps/vscode-extension/eslint.config.mjs
+++ apps/vscode-extension/eslint.config.mjs
@@ -1,5 +1,5 @@
-import typescriptEslint from "@typescript-eslint/eslint-plugin";
 import tsParser from "@typescript-eslint/parser";
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
 
 export default [
   {
@@ -28,7 +28,8 @@ export default [
       curly: "warn",
       eqeqeq: "warn",
       "no-throw-literal": "warn",
-      //semi: "warn",
+      "no-unused-vars": "error",
+      "sort-imports": "error",
     },
   },
 ];
