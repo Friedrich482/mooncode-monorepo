a89ef6c25841a64fe59206ebaa7fc703f74c21f1
Friedrich482 <friedricht109@gmail.com>
2024-12-29 23:50:31 +0100

    perf: strictNullChecks
    turned the strictNullChecks option in the tsconfig to true and added a test case in the auth service to check if the user exists before trying to access his password

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,8 @@
-import { Injectable, UnauthorizedException } from "@nestjs/common";
+import {
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from "@nestjs/common";
 import { JwtService } from "@nestjs/jwt";
 import { UsersService } from "src/users/users.service";
 @Injectable()
@@ -8,15 +12,17 @@ export class AuthService {
     private jwtService: JwtService,
   ) {}
 
-  async signIn(
-    username: string,
-    pass: string,
-  ): Promise<{ access_token: string }> {
+  async signIn(username: string, pass: string) {
     const user = await this.usersService.findOne(username);
-    if (user?.password !== pass) {
+
+    if (!user) throw new NotFoundException();
+
+    if (user.password !== pass) {
       throw new UnauthorizedException();
     }
+
     const payload = { sub: user.userId, username: user.username };
+
     return {
       access_token: await this.jwtService.signAsync(payload),
     };
