d491df9bf7b00ab163c82037a8d512ad8f2200db
Friedrich482 <friedricht109@gmail.com>
2024-12-29 23:31:01 +0100

    feat: authentication
    added the auth module, controller and service with a signIn returning a jwt access_token when the user username and password are correct.
    Used the config module and service to manage environment variables

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -26,8 +26,8 @@ export class UsersController {
   }
 
   @Get(":id")
-  findOne(@Param("id") id: string) {
-    return this.usersService.findOne(+id);
+  findOne(@Param("username") username: string) {
+    return this.usersService.findOne(username);
   }
 
   @Patch(":id")
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,8 +2,26 @@ import { CreateUserDto } from "./dto/create-user.dto";
 import { Injectable } from "@nestjs/common";
 import { UpdateUserDto } from "./dto/update-user.dto";
 
+export type User = {
+  userId: number;
+  username: string;
+  password: string;
+};
+
 @Injectable()
 export class UsersService {
+  private readonly users: User[] = [
+    {
+      userId: 1,
+      username: "john",
+      password: "changeme",
+    },
+    {
+      userId: 2,
+      username: "maria",
+      password: "guess",
+    },
+  ];
   create(createUserDto: CreateUserDto) {
     return "This action adds a new user";
   }
@@ -12,8 +30,8 @@ export class UsersService {
     return `This action returns all users`;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async findOne(username: string) {
+    return this.users.find((user) => user.username === username);
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
