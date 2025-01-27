46cd7081a774329877c332ca632cfdcf62ade5e0
Friedrich482 <friedricht109@gmail.com>
2025-01-28 00:47:08 +0100

    init: dashboard
    initialized the dashboard part of MoonCode with Vite+React, and installed must packages like tailwindcss and zod

---
+++ apps/dashboard/apps/dashboard/src/components/Header.tsx
@@ -0,0 +1,16 @@
+const Header = () => {
+  return (
+    <header>
+      Header
+      <nav>
+        <ul>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
---
+++ apps/dashboard/apps/dashboard/src/components/dashboard-page/Main.tsx
@@ -0,0 +1,5 @@
+const Main = () => {
+  return <div className="text-red-600">Main</div>;
+};
+
+export default Main;
