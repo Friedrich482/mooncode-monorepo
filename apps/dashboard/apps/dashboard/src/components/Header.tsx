812129246905f19e815626206224aa680fab017c
Friedrich482 <friedricht109@gmail.com>
2025-01-29 18:20:12 +0100

    fix: tailwindCSS
    downgraded to tailwindCSS version 3.4.17 and installed shadcn in the project

--- apps/dashboard/apps/dashboard/src/components/Header.tsx
+++ apps/dashboard/apps/dashboard/src/components/Header.tsx
@@ -1,17 +1,19 @@
+import { Button } from "./ui/button";
+
 const Header = () => {
   // make an array for links
   return (
-    <header className="flex text-3xl gap-3 w-full justify-between font-geist">
-      Logo here
-      <nav className="flex">
-        <ul className="flex items-center justify-center gap-4 pr-10">
+    <header className="flex text-3xl gap-3 w-full font-geist pb-10">
+      <div>Logo here</div>
+      <nav className="flex-1">
+        <ul className="flex items-center justify-end gap-4 pr-10">
           <li>
-            <a href="">Dashboard</a>
-          </li>
-          <li>
-            <a href="">Profile</a>
+            <Button asChild>
+              <a>Dashboard</a>
+            </Button>
           </li>
         </ul>
+        <div></div>
       </nav>
     </header>
   );
