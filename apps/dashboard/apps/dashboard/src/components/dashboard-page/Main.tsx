c99482320f0c88016f2e942409388841c75fef55
Friedrich482 <friedricht109@gmail.com>
2025-02-03 01:37:55 +0100

    refactor: time spent today
    extract all operations related to the time spent today in its own component

--- apps/dashboard/apps/dashboard/src/components/dashboard-page/Main.tsx
+++ apps/dashboard/apps/dashboard/src/components/dashboard-page/Main.tsx
@@ -1,26 +1,9 @@
-import TimeSpentTodaySkeleton from "../ui/skeleton/TimeSpentTodaySkeleton";
-import fetchTimeSpentToday from "@/utils/fetchTimeSpentToday";
-import { useQuery } from "@tanstack/react-query";
+import TimeSpentToday from "./TimeSpentToday";
 
 const Main = () => {
-  // extract it in a custom hook
-  const { isPending, error, data } = useQuery({
-    queryKey: ["timeSpentToday"],
-    queryFn: fetchTimeSpentToday,
-  });
-
   return (
     <main className="px-14 text-black dark:text-white">
-      <h1 className="flex items-center justify-center gap-4 text-2xl">
-        <span>Today: </span>
-        {isPending && <TimeSpentTodaySkeleton />}
-        {error && (
-          <span className="text-red-600">
-            An error occurred: {error.message}
-          </span>
-        )}
-        {data && `${data} seconds`}
-      </h1>
+      <TimeSpentToday />
     </main>
   );
 };
