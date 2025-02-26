70fa26b09f8ae44efd5b2932a9fc7daf5b950728
Friedrich482 <friedricht109@gmail.com>
2025-02-27 00:50:36 +0100

    fix: eslint
    fixed the issue of relative paths with eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   parser: "@typescript-eslint/parser",
   parserOptions: {
-    project: "apps/api/tsconfig.json",
+    project: "tsconfig.json",
 
     sourceType: "module",
   },
