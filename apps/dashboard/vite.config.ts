ec6d6208eaa2d0d6ef8960780804df7716456012
Friedrich482 <friedricht109@gmail.com>
2025-03-15 23:55:33 +0100

    feat: trpc
    implemented  trpc on the client side

--- apps/dashboard/vite.config.ts
+++ apps/dashboard/vite.config.ts
@@ -1,16 +1,24 @@
+import commonjs from "vite-plugin-commonjs";
 import { defineConfig } from "vite";
 import path from "path";
 import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), commonjs()],
   server: {
     port: 4208,
   },
-
+  optimizeDeps: {
+    include: ["react", "react-dom", "@tanstack/react-query"],
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+      "@tanstack/react-query": path.resolve(
+        __dirname,
+        "./node_modules/@tanstack/react-query",
+      ),
     },
+    dedupe: ["react", "react-dom"],
   },
 });
