927f48ae91b5fc68b2d95a870ebc60215170d12a
Friedrich482 <friedricht109@gmail.com>
2025-03-17 00:40:28 +0100

    refactor: fetch functions
    removed all fetching data functions because we no longer need them (thanks to trpc) and also all related schemas

--- apps/dashboard/src/types-schemas.ts
+++ apps/dashboard/src/types-schemas.ts
@@ -1,22 +1,3 @@
-import { z } from "zod";
-
-export const dailyPeriodSchema = z.object({
-  timeSpent: z.number().min(0),
-  dayLanguagesTime: z.record(z.string().min(1), z.number().min(0)),
-});
-
-export const weeklyPeriodSchema = z.object({
-  timeSpent: z.number().min(0),
-  weekLanguagesTime: z.record(z.string().min(1), z.number().min(0)),
-  daysOfWeekStats: z.record(
-    z.string().min(2),
-    z.object({
-      timeSpent: z.number().min(0),
-      languages: z.record(z.string().min(1), z.number().min(0)),
-    }),
-  ),
-});
-
 export type Period =
   | "Today"
   | "Yesterday"
@@ -32,43 +13,27 @@ export type WeeklyPeriod = "Past week" | "This week";
 export const periodConfig = {
   "Past week": {
     offset: 1,
-    route: "weekly",
-    schema: weeklyPeriodSchema,
   },
   Today: {
     offset: 0,
-    route: "daily",
-    schema: dailyPeriodSchema,
   },
   Yesterday: {
     offset: 1,
-    route: "daily",
-    schema: dailyPeriodSchema,
   },
   "Last 3 days": {
     offset: 3,
-    route: "daily",
-    schema: dailyPeriodSchema,
   },
   "This week": {
     offset: 0,
-    route: "weekly",
-    schema: weeklyPeriodSchema,
   },
   "Past 2 weeks": {
     offset: 2,
-    route: "weekly",
-    schema: weeklyPeriodSchema,
   },
   "This month": {
     offset: 0,
-    route: "monthly",
-    schema: weeklyPeriodSchema,
   },
   "Previous month": {
     offset: 1,
-    route: "monthly",
-    schema: weeklyPeriodSchema,
   },
 } as const;
 
--- apps/dashboard/src/utils/getAuthToken.ts
+++ apps/dashboard/src/utils/getAuthToken.ts
@@ -1,7 +1,12 @@
 import { z } from "zod";
 
 const getAuthToken = () => {
-  return z.string().min(1).parse(localStorage.getItem("auth-token"));
+  try {
+    return z.string().min(1).parse(localStorage.getItem("auth-token"));
+  } catch (error) {
+    console.error(error);
+    return "";
+  }
 };
 
 export default getAuthToken;
