3230cbefc3d0fe42adcb4dcaa137b2dec0ad411a
Friedrich482 <friedricht109@gmail.com>
2025-03-31 23:48:36 +0200

    fix: chart - created a specific method called getDailyStatsForChart in the coding-data.service.ts - created the corresponding query procedure -  got exactly the data needed in the TodayLanguagesChart, and added the colors and the languageName to display the chart

--- apps/dashboard/src/components/dashboard-page/charts/TodayLanguagesChart.tsx
+++ apps/dashboard/src/components/dashboard-page/charts/TodayLanguagesChart.tsx
@@ -1,12 +1,4 @@
-import {
-  Bar,
-  BarChart,
-  CartesianGrid,
-  Cell,
-  LabelList,
-  XAxis,
-  YAxis,
-} from "recharts";
+import { Bar, BarChart, CartesianGrid, Cell, XAxis, YAxis } from "recharts";
 import {
   ChartContainer,
   ChartTooltip,
@@ -15,14 +7,15 @@ import {
 import { DEFAULT_COLOR, chartConfig } from "@/constants";
 import CustomChartToolTip from "@/components/ui/custom-chart-tool-tip";
 import { Skeleton } from "@/components/ui/skeleton";
-import formatDuration from "@repo/utils/formatDuration";
 import languagesAttributes from "@/colors.json";
 import { trpc } from "@/utils/trpc";
 
 const TodayLanguagesChart = () => {
-  const { data, error, isLoading } = trpc.codingData.getDailyStats.useQuery({
-    offset: 0,
-  });
+  const { data, error, isLoading } =
+    trpc.codingData.getDailyStatsForChart.useQuery({
+      offset: 0,
+    });
+
   if (error) {
     return (
       <span className="text-red-500">An error occurred: {error.message}</span>
@@ -31,24 +24,21 @@ const TodayLanguagesChart = () => {
   if (isLoading) {
     return <Skeleton className="h-[24rem] w-[45%] max-chart:w-full" />;
   }
-  const { dayLanguagesTime, timeSpent } = data;
-  const chartData = Object.entries(dayLanguagesTime)
-    .map(([languageId, timeSpent]) => ({
-      languageId,
-      timeSpent,
-      color:
-        languagesAttributes[languageId as keyof typeof languagesAttributes]
-          .color || DEFAULT_COLOR,
-      languageName:
-        languagesAttributes[languageId as keyof typeof languagesAttributes]
-          .name,
-      value: formatDuration(timeSpent),
-    }))
-    .sort((a, b) => b.timeSpent - a.timeSpent);
-  const formattedTotalTimeSpent = formatDuration(timeSpent);
+
+  const { finalData, formattedTotalTimeSpent } = data;
+
+  const chartData = finalData.map((entry) => ({
+    ...entry,
+    color:
+      languagesAttributes[entry.languageId as keyof typeof languagesAttributes]
+        .color || DEFAULT_COLOR,
+    languageName:
+      languagesAttributes[entry.languageId as keyof typeof languagesAttributes]
+        .name,
+  }));
 
   return (
-    <div className="flex min-h-96 w-[45%] flex-col rounded-md border border-neutral-600/50 pb-2 max-chart:w-full">
+    <div className="flex min-h-96 w-[45%] flex-col gap-y-2 rounded-md border border-neutral-600/50 py-2 max-chart:w-full">
       <h2 className="text-center text-2xl font-bold">
         Today : {formattedTotalTimeSpent}
       </h2>
@@ -81,20 +71,14 @@ const TodayLanguagesChart = () => {
                 parseInt(value),
                 payload.color,
                 payload.languageId,
+                payload.percentage,
               );
             }}
           />
           <Bar dataKey="timeSpent" layout="vertical" radius={5}>
             {chartData.map((entry) => (
-              <Cell fill={entry.color} min={0} />
+              <Cell fill={entry.color} min={0} key={entry.languageId} />
             ))}
-            <LabelList
-              dataKey="value"
-              position="right"
-              offset={8}
-              className="fill-foreground"
-              fontSize={12}
-            />
           </Bar>
         </BarChart>
       </ChartContainer>
