2aa37be8e9954b894ddb3061a71e862b7f73a6c6
Friedrich482 <friedricht109@gmail.com>
2025-04-03 23:03:19 +0200

    feat: chart - added the fully functional calendar (date-picker) in a popover - added the shadcn popover and made some style changes in the classnames - created the utils functions: getPrevDayDate and getNextDayDate to handle the navigation buttons on the chart - added the util function getDateOffset

--- apps/dashboard/src/components/dashboard-page/charts/dayLanguagesChart/ChartTitle.tsx
+++ apps/dashboard/src/components/dashboard-page/charts/dayLanguagesChart/ChartTitle.tsx
@@ -1,30 +1,80 @@
 import { ChevronLeft, ChevronRight } from "lucide-react";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { Button } from "@/components/ui/button";
+import { Calendar } from "@/components/ui/calendar";
 import Icon from "@/components/ui/Icon";
+import { cn } from "@/lib/utils";
+import { useState } from "react";
 
 const ChartTitle = ({
-  date,
+  displayDate,
   formattedTotalTimeSpent,
   handleChevronLeftClick,
   handleChevronRightClick,
   offset,
+  date,
+  setDate,
 }: {
   handleChevronLeftClick: () => void;
   handleChevronRightClick: () => void;
   formattedTotalTimeSpent: string;
-  date: string;
+  displayDate: string;
   offset: number;
-}) => (
-  <h2 className="flex items-center justify-between gap-4 px-3 text-center text-2xl font-bold">
-    <Icon Icon={ChevronLeft} onClick={handleChevronLeftClick} />
-    <p>
-      {formattedTotalTimeSpent} - {date}
-    </p>
-    <Icon
-      Icon={ChevronRight}
-      onClick={handleChevronRightClick}
-      disabled={offset === 0}
-    />
-  </h2>
-);
+  date: Date;
+  setDate: React.Dispatch<React.SetStateAction<Date>>;
+}) => {
+  const [isPopoverOpen, setIsPopoverOpen] = useState(false);
+  const handleClick = () => setIsPopoverOpen((prev) => !prev);
+  return (
+    <h2 className="flex items-center justify-between gap-4 px-3 text-center text-2xl font-bold">
+      <Icon Icon={ChevronLeft} onClick={handleChevronLeftClick} />
+      <div className="relative">
+        {formattedTotalTimeSpent} -{" "}
+        <Popover
+          open={isPopoverOpen}
+          onOpenChange={setIsPopoverOpen}
+          defaultOpen={true}
+        >
+          <PopoverTrigger asChild>
+            <Button
+              variant="link"
+              onClick={handleClick}
+              className="p-0 text-2xl"
+            >
+              {displayDate}
+            </Button>
+          </PopoverTrigger>
+          <PopoverContent sideOffset={4}>
+            <Calendar
+              mode="single"
+              selected={date}
+              onSelect={(e) => {
+                setDate(e || date);
+                setIsPopoverOpen(false);
+              }}
+              className="p-2"
+              classNames={{
+                day_today: cn(
+                  "shadow-sm shadow-moon",
+                  date.getDate() === new Date().getDate() && "shadow-none",
+                ),
+              }}
+              disabled={{ after: new Date() }}
+            />
+          </PopoverContent>
+        </Popover>
+      </div>
+      <Icon
+        Icon={ChevronRight}
+        onClick={handleChevronRightClick}
+        disabled={offset === 0}
+      />
+    </h2>
+  );
+};
 
 export default ChartTitle;
