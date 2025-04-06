8dfaa5d25b5b5fb2300d564188fd212de5a750c5
Friedrich WT <friedricht109@gmail.com>
2025-04-06 23:53:36 +0200

    Update webpack.config.ts

--- apps/vscode-extension/webpack.config.ts
+++ apps/vscode-extension/webpack.config.ts
@@ -7,7 +7,7 @@ const path = require("path");
 //@ts-check
 /** @typedef {import('webpack').Configuration} WebpackConfig **/
 
-/** @type 2WebpackConfig */
+/** @type WebpackConfig */
 const extensionConfig = {
   target: "node", // VS Code extensions run in a Node.js-context 📖 -> https://webpack.js.org/configuration/node/
   mode: "none", // this leaves the source code as close as possible to the original (when packaging we set this to 'production')
