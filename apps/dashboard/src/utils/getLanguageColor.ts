8972830899f60b551adc55b5cad63454551125bd
Friedrich482 <friedricht109@gmail.com>
2025-04-11 17:41:22 +0200

    fix: colors - replaced all null colors in the colors.json file -  updated the getLanguageColor function in consequence

--- apps/dashboard/src/utils/getLanguageColor.ts
+++ apps/dashboard/src/utils/getLanguageColor.ts
@@ -1,8 +1,7 @@
 import { DEFAULT_COLOR } from "@/constants";
 import languagesAttributes from "@/colors.json";
 const getLanguageColor = (languageId: string) => {
-  // TODO fix (string | null => string) this by adding colors to the colors.json file for languages like "COBOL"
-  let languageColor: string | null = "";
+  let languageColor: string = "";
   try {
     languageColor =
       languagesAttributes[languageId as keyof typeof languagesAttributes].color;
@@ -10,7 +9,7 @@ const getLanguageColor = (languageId: string) => {
     console.error(error);
     languageColor = DEFAULT_COLOR;
   }
-  return languageColor || DEFAULT_COLOR;
+  return languageColor;
 };
 
 export default getLanguageColor;
