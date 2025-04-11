2af6d16766e1500c2f3994688d3670b5e2d4c700
Friedrich482 <friedricht109@gmail.com>
2025-04-11 21:06:23 +0200

    fix: countStrictWeeks
    improved the countStrictWeeks function by avoiding mutating the input date

--- apps/api/src/utils/countStrictWeeks.ts
+++ apps/api/src/utils/countStrictWeeks.ts
@@ -1,19 +1,15 @@
+import { addDays, isBefore, isEqual } from "date-fns";
 import { endOfWeek, startOfWeek } from "date-fns";
 
-import { endOfWeek, startOfWeek } from "date-fns";
-import { addDays, isBefore, isEqual } from "date-fns";
- 
 const countStrictWeeks = (start: Date, end: Date) => {
   let weeks = 0;
   // Create a copy to avoid mutating the input date
   let current = startOfWeek(new Date(start));
   const endDate = new Date(end);
- 
+
   while (isBefore(current, endDate) || isEqual(current, endDate)) {
     weeks++;
-    // Get end of current week
     const weekEnd = endOfWeek(current);
-    // Set current to the start of next week (add one day to week end)
     current = startOfWeek(addDays(weekEnd, 1));
   }
   return weeks;
