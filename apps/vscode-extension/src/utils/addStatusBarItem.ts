206781528b65246d7b338cc7f0a60f92b1e4a35f
Friedrich482 <friedricht109@gmail.com>
2025-07-05 17:42:08 +0200

    refactor: update import - statements to use default import for vscode

--- apps/vscode-extension/src/utils/addStatusBarItem.ts
+++ apps/vscode-extension/src/utils/addStatusBarItem.ts
@@ -1,4 +1,4 @@
-import * as vscode from "vscode";
+import vscode from "vscode";
 const addStatusBarItem = () => {
   const statusBarItem = vscode.window.createStatusBarItem(
     vscode.StatusBarAlignment.Left,
