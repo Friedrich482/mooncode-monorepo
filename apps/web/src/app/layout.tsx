22efeff7ca0cb18b76db0aaff82fc40dba31bb1a
Friedrich482 <friedricht109@gmail.com>
2025-07-15 18:25:25 +0200

    init: web app
    init web app (NextJS)

---
+++ apps/web/src/app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { ThemeProvider } from "next-themes";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "MoonCode",
+  description: "Track you coding time, languages and files with ease",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${inter.className} antialiased`}>
+        <ThemeProvider enableSystem>{children}</ThemeProvider>
+      </body>
+    </html>
+  );
+}
---
+++ apps/web/src/app/page.tsx
@@ -0,0 +1,5 @@
+const Home = () => {
+  return <div>MoonCode</div>;
+};
+
+export default Home;
